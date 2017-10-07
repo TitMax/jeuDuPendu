49ecb6f46514e730a079ba0b8431e5254a65b0b2
TitMax <maxime.boussaingault@gmail.com>
2017-10-07 22:34:02 +0200

    Ajouté le fichier dico.h

---
+++ dico.h
@@ -0,0 +1,17 @@
+/*
+dico.h
+------------
+
+Par (nom de l'auteur du programme)
+Date de création :  29/09/2017
+Date de la dernière modification : 29/09/2017
+
+Rôle : Prototype de dico.c
+*/
+
+#ifndef DICO_H
+#define DICO_H
+int motAleatoire (char *motSecret);
+
+#endif // DICO_H
+
