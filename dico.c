85c684a9a2dc817b906e3161f31267e68e052bc0
TitMax <maxime.boussaingault@gmail.com>
2017-10-07 22:34:11 +0200

    Ajouté le fichier dico.c

---
+++ dico.c
@@ -0,0 +1,64 @@
+/*
+dico.c
+------------
+
+Par (nom de l'auteur du programme)
+Date de création :  29/09/2017
+Date de la dernière modification : 29/09/2017
+
+Rôle : Fonction pour trouver un mot aléatoire dans le dico
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "dico.h"
+
+#define TAILLE_MAX 100
+
+int motAleatoire (char *motSecret)
+{
+    int i = 0, nombreDeMots = 0, roulette = 0, caractereActuel = 0;
+
+    FILE* dico = NULL;
+
+    dico = fopen("dico.txt", "r");
+
+    if (dico != NULL)
+    {
+        // On compte le nombre de mots présent dans le fichier
+        do
+        {
+            caractereActuel = fgetc(dico);
+            if (caractereActuel == '\n')
+            nombreDeMots++;
+        } while (caractereActuel != EOF);
+
+        // On génère un nombre aléatoire
+        srand(time(NULL));
+        roulette = rand() % nombreDeMots;
+
+        rewind(dico);   // On retourne au début du fichier
+
+        while (i < roulette)
+        {
+            caractereActuel = fgetc(dico);
+            if (caractereActuel == '\n')
+            {
+                i++;
+            }
+        }
+        fgets (motSecret, TAILLE_MAX, dico);
+
+        motSecret[strlen(motSecret) - 1] = '\0';
+
+        fclose(dico);
+        return 1;
+    }
+    else
+    {
+        printf("Impossible de continuer, dictionnaire inexistant.\n");
+        exit(0);
+    }
+}
